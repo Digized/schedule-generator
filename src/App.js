df32ad6aba613aec4868eec5e39e7a14667d2f5b
Zuraiz Zafar <zuraiz.zafar97@gmail.com>
2017-11-02 19:58:58 -0400

    init + 📆

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Calendar} from "./Calendar"
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Calendar />
+      </div>
+    );
+  }
+}
+
+export default App;
