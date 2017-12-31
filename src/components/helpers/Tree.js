69ba66be08a643b83e9c0de1e766ef51bf58e606
Zuraiz Zafar <zuraiz.zafar97@gmail.com>
2017-12-31 00:34:16 -0500

    new styling and fixed algorithm

--- src/components/helpers/Tree.js
+++ src/components/helpers/Tree.js
@@ -1,250 +1,4 @@
 import { week } from "../constants";
-
-class SectionTree {
-
-    constructor(section) {
-        this.activities = section.activities;
-        this._root = new Node(section);
-        this.treeData = {};
-        this.meta = {};
-        this.fill();
-    }
-
-    fill() {
-        const _this = this
-        this.activities.forEach(function (element) {
-            // _this._time(element);
-            if (_this.treeData[element.activity] === undefined) {
-                _this.treeData[element.activity] = [];
-            }
-            _this.treeData[element.activity].push(element)
-        });
-        this.makeTree();
-    }
-
-    //LEC -> DGD -> TUT -> LAB
-    makeTree() {
-        let activeNode = [this._root];
-        if (this.treeData["LEC"]) {
-            activeNode = activeNode[0];
-            this.meta["LECnodes"] = [];
-            this.treeData["LEC"] && this.treeData["LEC"].forEach(lec => {
-                const node = new Node(lec, "LEC");
-                node.parent = activeNode;
-                activeNode.children.push(node);
-                activeNode = node;
-                this.meta["LECnodes"].push(lec);
-                this.meta["lastLECNode"] = [activeNode];
-            });
-        }
-
-        //should be simplified.
-        activeNode = this.meta.lastLECNode || activeNode;
-        if (this.treeData["DGD"]) {
-            this.meta["DGDnodes"] = [];
-            activeNode.forEach(actvnode => {
-                this.treeData["DGD"].forEach(dgd => {
-                    const node = new Node(dgd);
-                    node.parent = actvnode;
-                    actvnode.children.push(node);
-                    this.meta["DGDnodes"].push(node);
-                });
-            });
-        }
-
-        activeNode = this.meta["DGDnodes"] || this.meta.lastLECNode || activeNode;
-        if (this.treeData["TUT"]) {
-            this.meta["TUTnodes"] = [];
-            activeNode.forEach(actvnode => {
-                this.treeData["TUT"].forEach(dgd => {
-                    const node = new Node(dgd);
-                    node.parent = actvnode;
-                    actvnode.children.push(node);
-                    this.meta["TUTnodes"].push(node);
-                });
-            });
-        }
-
-        activeNode = this.meta["TUTnodes"] || this.meta["DGDnodes"] || this.meta.lastLECNode || activeNode;
-        if (this.treeData["LAB"]) {
-            this.meta["LABnodes"] = [];
-            activeNode.forEach(actvnode => {
-                this.treeData["LAB"].forEach(lab => {
-                    const node = new Node(lab);
-                    node.parent = actvnode;
-                    actvnode.children.push(node);
-                    this.meta["LABnodes"].push(node);
-                });
-            });
-
-        }
-
-        this.verifyAndDelete();
-    }
-
-
-    verifyAndDelete() {
-        const leaves = this.meta["LABnodes"] || this.meta["TUTnodes"] || this.meta["DGDnodes"] || this.meta.lastLECNode || [this._root];
-        leaves.forEach((leaf, index, object) => {
-            let activeNode = leaf;
-            const times = [];
-            while (activeNode !== this._root) {
-                let [startHour, startMin] = activeNode.elem.start.split(":");
-                let [endHour, endMin] = activeNode.elem.end.split(":");
-                times.forEach(time => { // eslint-disable-line no-loop-func
-                    if (time.day !== activeNode.elem.day) {
-                        return;
-                    }
-                    if (parseFloat(time.startHour) + parseFloat(time.startMin) / 100 >= parseFloat(endHour) + parseFloat(endMin) / 100
-                        || parseFloat(time.endHour) + parseFloat(time.endMin) / 100 >= parseFloat(startHour) + parseFloat(startMin) / 100) {
-                        times.push({ day: activeNode.elem.day, startHour, startMin, endHour, endMin });
-                        return;
-                    } else {
-                        leaf.parent.children.splice(leaf.parent.children.indexOf(leaf), 1);
-                        leaf.parent = null;
-                        object.splice(index, 1);
-                    }
-
-                });
-                if (times.length === 0) {
-                    times.push({ day: activeNode.elem.day, startHour, startMin, endHour, endMin })
-                }
-
-                activeNode = activeNode.parent;
-            }
-        });
-    }
-
-    generate() {
-        const combinations = []
-        const leaves = this.meta["LABnodes"] || this.meta["TUTnodes"] || this.meta["DGDnodes"] || this.meta.lastLECNode || [this._root];
-        leaves.forEach(leaf => {
-            let activeNode = leaf;
-            const combination = [];
-            while (activeNode !== this._root) {
-                combination.push(activeNode.elem);
-                activeNode = activeNode.parent;
-            }
-            combination.reverse();
-            combinations.push({ combination, professor: this._root.elem.professor, section: this._root.elem.section });
-        })
-        return combinations;
-    }
-
-}
-
-class CourseTree {
-    constructor(course) {
-        this.course = new Node(course);
-        this.meta = {
-            code: course.course_code,
-            name: course.course_title,
-        }
-        this.sections = []
-        course.sections.forEach(section => {
-            let tSection = new SectionTree(section);
-            tSection._root.parent = this.course;
-            this.course.children.push(tSection._root);
-            this.sections.push(tSection);
-        });
-    }
-
-    generate() {
-        let combinations = [];
-        this.sections.forEach(section => {
-            combinations = combinations.concat(section.generate());
-        });
-        return { info: this.meta, combinations };
-    }
-
-}
-
-export default class ScheduleTree {
-    constructor(courses) {
-        this.courseSegment = {};
-        this._root = new Node("ROOT");
-        const _this = this;
-        this.schedules = [];
-        courses.forEach(course => {
-            if (course.fail === true) {
-                return;
-            }
-
-            _this.courseSegment[course.course_code] = []
-            new CourseTree(course).generate().combinations.forEach(combination => {
-                _this.courseSegment[course.course_code].push(new Node(combination));
-            });
-        });
-        this.generate();
-    }
-
-    generate() {
-        let keys = Object.keys(this.courseSegment);
-        let activeNode = [this._root];
-        keys.forEach(key => {
-            let next = []
-            activeNode.forEach(actvNode => {
-                this.courseSegment[key].forEach(comb => {
-                    actvNode.children.push(comb);
-                    comb.parent = actvNode;
-                    next.push(comb);
-                });
-            })
-            activeNode = next;
-        });
-
-        const leaves = activeNode;
-
-        let myLeaves = leaves;
-        for (let i = 0; i < leaves.length; i++) {
-            let leaf = leaves[i];
-            let currNode = leaf;
-            let times = {};
-            while (currNode !== this._root) {
-                currNode.elem.combination.forEach(comb => { // eslint-disable-line no-loop-func
-                    if (times[`${comb.day}${comb.start}`] === undefined) {
-                        times[`${comb.day}${comb.start}`] = comb; // eslint-disable-line no-loop-func
-                    } else {
-                        myLeaves[myLeaves.indexOf(leaf)] = null;
-                    }
-                });
-                currNode = currNode.parent;
-            }
-
-        }
-        myLeaves = myLeaves.filter(n => n);
-        this.schedules = myLeaves;
-    }
-
-    forCalendar() {
-        const scheduleList = [];
-        this.schedules.length > 0 && this.schedules.forEach(leaf => {
-            let activeNode = leaf;
-            const schedule = [];
-            while (activeNode !== this._root) {
-                let course_code;
-                Object.keys(this.courseSegment).forEach(course => { // eslint-disable-line no-loop-func
-                    if (this.courseSegment[course].indexOf(activeNode) > -1) {
-                        course_code = course; // eslint-disable-line no-loop-func
-                    }
-                })
-                activeNode.elem.combination.forEach(activity => {
-                    let [startHour, startMin] = activity.start.split(":");
-                    let [endHour, endMin] = activity.end.split(":");
-                    schedule.push({
-                        start: week.day(activity.day).hour(parseInt(startHour, 10)).minute(parseInt(startMin, 10)).toDate(),
-                        end: week.day(activity.day).hour(parseInt(endHour, 10)).minute(parseInt(endMin, 10)).toDate(),
-                        title: `${activity.activity} | ${course_code} | ${activity.location}`
-                    })
-                })
-                activeNode = activeNode.parent;
-            }
-            scheduleList.push(schedule);
-        });
-        return scheduleList;
-    }
-}
-
 class Node {
     constructor(elem) {
         this.elem = elem;
@@ -253,3 +7,196 @@ class Node {
     }
 }
 
+class Tree {
+
+    constructor() {
+        this.leaves = [];
+        this._leaves = [];
+    }
+    print(level,root) {
+        let s = "";
+        for (let i = 1; i < level; i++) {
+            s += "\t\t"
+        }
+        console.log(s, JSON.stringify(root.elem));
+        root.children.forEach(child => {
+            this.print(level + 1, child);
+        })
+    }
+
+    verifyAndDelete() {
+        const validLeaves = [];
+        const _this = this;
+        this._leaves.forEach(leaf => {
+            let times = {};
+            let actvNode = leaf;
+            let failed = false;
+            while (actvNode !== _this.root) {
+                const day = actvNode.elem.day;
+                const [startHr, startMin] = actvNode.elem.start.split(":").map(Number);
+                const [endHr, endMin] = actvNode.elem.end.split(":").map(Number);
+                const actvLength = (endHr * 60 + endMin) - (startHr * 60 + startMin);
+                if (actvLength <= 90 && times[`${day}${startHr * 60 + startMin}`] === undefined) { // 1.5 hr
+                    times[`${day}${startHr * 60 + startMin}`] = actvNode;
+                    actvNode = actvNode.parent;
+
+                } else if (actvLength > 90 && times[`${day}${startHr * 60 + startMin}`] === undefined &&
+                    times[`${day}${startHr * 60 + startMin + 90}`] === undefined) { // 3hr activity
+                    times[`${day}${startHr * 60 + startMin}`] = actvNode;
+                    times[`${day}${startHr * 60 + startMin + 90}`] = actvNode;
+                    actvNode = actvNode.parent;
+
+
+                } else { //clash found delete leaf
+                    failed = true;
+                    actvNode = _this.root;
+                }
+            }
+            if (failed === false) {
+                validLeaves.push(leaf);
+            }
+        });
+        this.leaves = validLeaves;
+    }
+
+    getLeaves() {
+        return this.leaves;
+    }
+}
+
+class SectionTree extends Tree {
+
+    constructor(section, course) {
+        super();
+        this.activitySegregation = [];
+        this.root = new Node(section.section);
+        this.activities = section.activities;
+        const meta = {"courseCode":course.course_code, "courseName":course.course_title, "professor": section.professor || "boobies", "section":section.section};
+        this.activities.forEach(activity => {activity.meta = meta});
+        this.generateActivityTypes();
+        this.makeTree();
+        this.verifyAndDelete();
+    }
+
+    generateActivityTypes() {
+        const activityTypes = [];
+        const _this = this;
+        this.activities.forEach(activity => {
+            const activityName = activity.activity;
+            if (activityTypes.indexOf(activityName) < 0) {
+                activityTypes.push(activityName);
+                _this.activitySegregation[activityName] = [];
+                _this.activitySegregation[activityName].push(activity);
+
+            } else {
+                _this.activitySegregation[activityName].push(activity);
+            }
+        });
+
+    }
+
+    makeTree() {
+        let actvNodes = [this.root];
+        let node;
+        Object.keys(this.activitySegregation).forEach(key => {
+            if (key === "LEC") {
+                actvNodes.forEach(actvNode => {
+                    this.activitySegregation[key].forEach(lecture => {
+                        node = new Node(lecture);
+                        node.parent = actvNode;
+                        actvNode.children.push(node);
+                        actvNode = node;
+                    });
+                });
+                actvNodes = [node];
+            } else {
+                const segregatedNodes = [];
+                actvNodes.forEach(actvNode => {
+                    this.activitySegregation[key].forEach(activity => {
+                        const node = new Node(activity);
+                        node.parent = actvNode;
+                        actvNode.children.push(node);
+                        segregatedNodes.push(node);
+                    })
+                });
+                actvNodes = segregatedNodes;
+            }
+        });
+        this._leaves = actvNodes;
+
+    }
+
+}
+
+class CourseTree extends Tree {
+    constructor(course) {
+        super();
+        this.root = new Node(course);
+        this.leaves = [];
+        const _this = this;
+        course.sections.forEach(section => {
+            const sectionT = new SectionTree(section, course);
+            _this.root.children.push(...sectionT.root.children);
+            sectionT.root.children.forEach(child => {
+                child.parent = _this.root;
+            });
+            _this.leaves.push(...sectionT.getLeaves());
+        });
+    }
+}
+
+class ScheduleTree extends Tree {
+    constructor(courses) {
+        super();
+        this.root = new Node(courses);
+        this.segments = {};
+        this.courses = courses;
+
+        this.generateTree();
+        this.verifyAndDelete();
+        this.print(0, this.root);
+        // console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+    }
+
+    generateTree() {
+        let actveNodes = [this.root];
+        this.courses.forEach(course => {
+            const leaves = [];
+            actveNodes.forEach(actveNode => {
+                const courseT = new CourseTree(course);
+                actveNode.children.push(...courseT.root.children);
+                courseT.root.children.forEach(child => {
+                    child.parent = actveNode;
+                });
+                leaves.push(...courseT.getLeaves());
+            });
+            actveNodes = leaves;
+        });
+
+        this._leaves = actveNodes;
+    }
+
+    calendarify(){
+        const schedules = [];
+        this.leaves.forEach(leaf => {
+            let actveNode = leaf;
+            const schedule = [];
+            while(actveNode !== this.root){
+                let [startHour, startMin] = actveNode.elem.start.split(":").map(Number);
+                let [endHour, endMin] = actveNode.elem.end.split(":").map(Number);
+                schedule.push({
+                    start:week.day(actveNode.elem.day).hour(startHour).minute(startMin).toDate(),
+                    end:week.day(actveNode.elem.day).hour(endHour).minute(endMin).toDate(),
+                    title: `${actveNode.elem.activity} | ${actveNode.elem.meta.professor} | ${actveNode.elem.location} | ${actveNode.elem.meta.section} | ${actveNode.elem.meta.courseName}`
+                });
+                actveNode = actveNode.parent;
+            }
+            schedules.push(schedule);
+        });
+
+        return schedules;
+    }
+}
+
+export default ScheduleTree;
